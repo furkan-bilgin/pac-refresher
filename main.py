4ad58d5c7963d174aa058c1203713102206ffe6c
Furkan <info@furkanbilgin.net>
2023-08-26 22:10:07 +0300

    Added refreshment frequency to config

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from settings import settings
 PROXY_TEST_URL = "https://httpbin.org/status/200"
 PROXY_TIMEOUT_MS = 3000
 
-RUN_FREQUENCY_SECONDS = 30
-
 def test_proxy(proxy_url, proxy_protocol):
     try:
         r = requests.get(PROXY_TEST_URL, proxies={proxy_protocol: f"{proxy_protocol}://{proxy_url}"}, timeout=PROXY_TIMEOUT_MS)    
@@ -30,8 +28,8 @@ def start():
         with open(settings.config.pac_output_path, "w") as f:
             f.write(pac_output_text)
 
-        print("PAC Refresh done.")
-        time.sleep(RUN_FREQUENCY_SECONDS)
+        print(f"PAC has been refreshed with {len(working_proxies)} working proxies!")
+        time.sleep(settings.config.refresh_frequency_in_secs)
 
 if __name__ == "__main__":
     start()
--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ class Config:
     pac_template_path: str
     pac_output_path: str
     pac_proxy_placeholder: str
+    refresh_frequency_in_secs: int
     proxies: dict[str, list[str]]
 
 
