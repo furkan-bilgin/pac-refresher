fa5952f3abf8dff1726317ee4517810c2ebac5eb
Furkan <info@furkanbilgin.net>
2023-08-29 20:48:23 +0300

    stuff

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import json
+import re
 
 
 @dataclass(kw_only=True)
@@ -24,6 +25,28 @@ def refresh_settings():
         settings.config = Config(**json.load(f))
         with open(settings.config.pac_template_path) as f:
             settings.pac_template = f.read()
+
+        parse_proxies()
+
+
+def parse_proxies():
+    REGEX = r"{(?P<from>.+)->(?P<to>.+)}"
+    remove = []
+    for protocol, proxies in settings.config.proxies.items():
+        for proxy in proxies:
+            match = re.search(REGEX, proxy)
+            if not match:
+                continue
+
+            proxy_ids_from, proxy_ids_to = match.groups()
+            for i in range(int(proxy_ids_from), int(proxy_ids_to) + 1):
+                settings.config.proxies[protocol].append(
+                    proxy.replace(match.group(0), str(i)))
+
+            remove.append((proxies, proxy))
+
+    for list, item in remove:
+        list.remove(item)
 
 
 refresh_settings()
